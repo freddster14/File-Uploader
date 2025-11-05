94f4b0edfea5f740ba4058a9a26d6fb36eed8ff7
freddster14 <freddycarrillo951@gmail.com>
2025-11-04 18:33:01 -0600

    Replace bcrypt with bcryptjs for password hashing; add passport authentication setup

---
+++ passport.js
@@ -0,0 +1,41 @@
+const passport = require('passport');
+const bcrypt = require('bcryptjs');
+const LocalStrategy = require('passport-local').Strategy;
+const prisma = require('./prisma/client');
+
+passport.use(
+  new LocalStrategy(
+    { usernameField: 'email' },
+    async (username, password, done) => {
+    try {
+      const user = await prisma.user.findUnique({
+        where: {
+          username,
+        },
+      });
+      if (!user) return done(null, false, { msg: 'Username and password do not match' });
+      const match = await bcrypt.compare(password, user.password);
+      if (!match) return done(null, false, { msg: 'Username and password do not match' });
+      return done(null, user);
+    } catch (error) {
+      return done(error)
+    }
+  }),
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        id,
+      }
+    });
+    done(null, user)
+  } catch (error) {
+    done(error);
+  }
+});

5a6a3ea78efebe17752fa83cc0abcb62c4c6f509
freddster14 <freddycarrillo951@gmail.com>
2025-11-18 18:15:15 -0600

    Fix breadcrumbing logic and update navigation display

--- utils/breadCrumbs.js
+++ utils/breadCrumbs.js
@@ -4,15 +4,15 @@ async function breadcrumbing(id) {
   let current = await prisma.folder.findUnique({
     where: { id: parseInt(id, 10) },
   });
-  const res = [{ name: current.name, link: `/folder/${current.id}` }]
+  const res = []
   // go up the tree
   while(current.parentId !== null) {
+    res.push({ name: current.name, link: `/folder/${current.id}` });
     current = await prisma.folder.findUnique({
       where: {
         id: current.parentId,
       }
     })
-    res.push({ name: current.name, link: `/folder/${current.id}` });
   }
   return res;
 }
